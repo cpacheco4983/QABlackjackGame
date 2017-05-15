221d7de01be99b5100b6daa93d7d13361b1e4176
cpacheco <cpacheco4983@gmail.com>
2017-05-15 04:05:47 -0400

    Card: edit toString

--- src/Card.java
+++ src/Card.java
@@ -38,10 +38,19 @@ public class Card {
     public String toString() {
         switch (rank) {
             case 1: return "Ace of " + suit;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10: return rank + " of " + suit;
             case 11: return "Jack of " + suit;
             case 12: return "Queen of " + suit;
             case 13: return "King of " + suit;
-            default: return rank + " of " + suit;
+            default: return "Card???";
         }
     }
 }
